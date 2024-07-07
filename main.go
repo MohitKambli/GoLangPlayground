505a2038e906a00d2b6ad6fc62a312ca0ca0c632
Mohit Kambli <31406633+MohitKambli@users.noreply.github.com>
2024-07-07 19:08:03 -0400

    Added Interface

--- main.go
+++ main.go
@@ -161,6 +161,54 @@ func circleArea(radius float64) float64 {
 	}
 }*/
 
+
+
+
+
+
+/*
+// shape interface
+type shape interface {
+	area() float64
+	circumf() float64
+}
+
+type square struct {
+	length float64
+}
+type circle struct {
+	radius float64
+}
+
+// square methods
+func (s square) area() float64 {
+	return s.length * s.length
+}
+func (s square) circumf() float64 {
+	return s.length * 4
+}
+
+// circle methods
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c circle) circumf() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func printShapeInfo(s shape) {
+	fmt.Printf("area of %T is: %0.2f \n", s, s.area())
+	fmt.Printf("circumference of %T is: %0.2f \n", s, s.circumf())
+}
+*/
+
+
+
+
+
+
+
+
 func main() {
 	// Println Function
 	/*fmt.Println("Hello World")*/
@@ -383,4 +431,19 @@ func main() {
 	// Bill Initialization (Struct)
 	/*myBill := createBill()
 	promptOptions(myBill)*/
+
+	/*
+	// Interface
+	shapes := []shape{
+		square{length: 15.2},
+		circle{radius: 7.5},
+		circle{radius: 12.3},
+		square{length: 4.9},
+	}
+
+	for _, v := range shapes {
+		printShapeInfo(v)
+		fmt.Println("---")
+	}
+ 	*/
 }
